1347d5e9dd8284352fca9d7aecbe0e30526c27a9
Adrian Zabolitzki <adrianzab@gmail.com>
2025-04-18 15:05:52 -0600

    Create Navbar Link for Portfolio

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+export default function Navbar() {
+  const pathname = usePathname()
+
+  return (
+    <nav className="bg-gray-800 p-4">
+      <div className="max-w-7xl mx-auto flex justify-between">
+        <div className="flex items-center">
+          <Link href="/" className = "text-white text-xl font-bold">
+          My Portfolio
+          </Link>
+        </div>
+      </div>
+    </nav>
+  )
+}
